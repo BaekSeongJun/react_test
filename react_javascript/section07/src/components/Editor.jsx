af3d0b2d21fd93da60c18ed7fff8ceef84588c1f
BaekSeongJun <por9535@gmail.com>
2026-08-04 11:14:43 +0900

    2026-08-04 useReducer를 사용하여 상태값 관리하기

---
+++ react_javascript/section07/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import "../css/Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDownContent = (e) => {
+    //엔터를 입력했는지 점검
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="오늘의 할 일을 입력"
+        onChange={onChangeContent}
+        onKeyDown={onKeyDownContent}
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
